22528139942a324c1f892f16e687c7110e237520
Jonathan <jonsecu0001@hotmail.com>
2015-06-23 13:25:50 -0500

    QLearning funciona en linea.

--- src/program/Program.java
+++ src/program/Program.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package program;
 
 import action.Action;
@@ -25,44 +24,62 @@ public class Program {
      */
     public static void main(String[] args) {
         System.out.println("Inicio... ");
-        LineWorld lineworld = new LineWorld("myline",4);
+        LineWorld lineworld = new LineWorld("myline", 4);
         lineworld.setup();
         
-        lineworld.setOneReward(2, 3, (float) 1.0);
-        lineworld.setOneReward(3, 4, (float) 3.0);
-        lineworld.setOneReward(4, 4, (float) 1.0);
+        lineworld.setOneReward(1, 2, (float) 2.0);
+        lineworld.setOneReward(2, 1, (float) -1.0);
+        lineworld.setOneReward(2, 3, (float) 2.0);
+        lineworld.setOneReward(3, 2, (float) -1.0);
+        lineworld.setOneReward(3, 4, (float) 2.0);
+        lineworld.setOneReward(4, 3, (float) -1.0);
+        lineworld.setOneReward(4, 4, (float) 20.0);
         
-        Object initialState = 1;
-        
-        AMDP mdp = new AMDP(initialState,lineworld.getStates(),lineworld.getActions());
-        
-        QLearning q = new QLearning(0.5,0.9,1.0,Program.objectToString(lineworld.getValidMoves()));
-        HashMap<String,String> action = q.chooseAction(initialState.toString(), "epsilongreedy");
-        mdp.update("right", 10, "2");
-        
-        System.out.println(action);
-        System.out.println(mdp.getCurrentAction());
-        System.out.println(mdp.getCurrentState());
-        System.out.println(mdp.getCurrentReward());
+        System.out.println(lineworld.getRewards());
 
-        
+        Object initialState = 1;
+
+        AMDP mdp = new AMDP(initialState, lineworld.getStates(), lineworld.getActions());
+        QLearning q = new QLearning(0.5, 0.9, 1.0, Program.objectToString(lineworld.getValidMoves()));
+
+        String stateNow = initialState.toString();
+        HashMap<String, String> operation = q.chooseAction(stateNow, "epsilongreedy");
+
+        for (int i = 1; i < 30; i++) {
+
+            String action = operation.get("action");
+            String stateNext = operation.get("nextState");
+            System.out.println(lineworld.getRewards().get(Integer.parseInt(stateNow)).get(Integer.parseInt(stateNext)));
+            float reward = lineworld.getRewards().get(Integer.parseInt(stateNow)).get(Integer.parseInt(stateNext));
+            mdp.update(action, reward, stateNext);
+            q.updateQ(stateNow, action, reward, stateNext);
+
+            q.printQ();
+
+            stateNow = stateNext;
+            operation = q.chooseAction(stateNow, "epsilongreedy");
+
+        }
+
     }
-    
+
     /**
-     * Helper: convierte HashMap<Object,ArrayList<Object>> a HashMap<String,ArrayList<String>>
-     * @param objects
-     * @return 
+     * Helper: convierte HashMap<Object,ArrayList<Object>> a
+     * HashMap<String,ArrayList<String>> @param ob
+     *
+     * jects
+     * @return
      */
-    public static HashMap<String,HashMap<String,String>> objectToString(HashMap<Object,HashMap<Object,String>> objects){
-        HashMap<String,HashMap<String,String>> list = new HashMap<>();
-        for(Object o : objects.keySet()){
-            HashMap<String,String> a = new HashMap<>();
-            for(Object p : objects.get(o).keySet()){
-                a.put(p.toString(),objects.get(o).get(p));
+    public static HashMap<String, HashMap<String, String>> objectToString(HashMap<Object, HashMap<Object, String>> objects) {
+        HashMap<String, HashMap<String, String>> list = new HashMap<>();
+        for (Object o : objects.keySet()) {
+            HashMap<String, String> a = new HashMap<>();
+            for (Object p : objects.get(o).keySet()) {
+                a.put(p.toString(), objects.get(o).get(p));
             }
             list.put(o.toString(), a);
         }
         return list;
     }
-    
+
 }
